68a4f9dec9838cc7ed6525a9989b451b562bc716
Cormacmcs <Cormacmcs@hottab.net>
2018-01-03 16:17:49 +0000

    linting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './index.css'
 import numeral from 'numeral';
 
 const value = numeral(1000).format('$0,0.00');
-console.log(`the value is ${value}`);
+console.log(`the value is ${value}`); //eslint-disable-line no-console
 
 
