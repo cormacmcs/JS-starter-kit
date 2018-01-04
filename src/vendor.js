fb23c4a787acba78b31718919724240b9e507048
Cormacmcs <Cormacmcs@hottab.net>
2018-01-04 23:07:56 +0000

    bundle spliting and hashing

---
+++ src/vendor.js
@@ -0,0 +1,9 @@
+/*
+  This file is used to reference the vendor libraries used in this project.
+  This is for webpack prod builds only.
+  All libraries referenced here will be written to vendor.js
+*/
+
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,20 +1,49 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+    //Generate a seperate css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    //hash files so name changes when content changes
+    new WebpackMd5Hash(),
+    //Create seperate bundles so can be cached seperatly
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    //Create Html file that includes reference to bundle js
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: { // html minify options
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true
+    }),
     //Eliminate duplicate packages
     new webpack.optimize.DedupePlugin(),
     //Minify js
@@ -23,7 +52,7 @@ export default {
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
-      { test: /\.css$/, loaders: ['style', 'css'] }
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap') }
     ]
   }
 }
